17b0a618b739316a2cee5b5c2d4a39aaf837e701
Daria <riphlogiston@gmail.com>
2022-05-19 16:21:46 +0600

    change db to postgresql

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'snake_news',
+        'User': 'daria',
+        'PASSWORD': '1029384756',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
